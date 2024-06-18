5647ec976bacf1ccc85eae7eea703fb70c7eb268
Christophe <cguegan@gmail.com>
2024-06-18 13:59:38 +0200

    Initial Commit

---
+++ iColregs/iColregsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iColregsApp.swift
+//  iColregs
+//
+//  Created by Christophe Guégan on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct iColregsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
